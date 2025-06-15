3cfb52b18e05c05a11724390164baa28bb2ce816
luizinho2008 <luizhenriquemoreiracouto@gmail.com>
2025-06-15 12:23:18 -0300

    Adicionando as Views

--- CFM/app/src/main/java/com/example/cf/TelaInicial.java
+++ CFM/app/src/main/java/com/example/cf/TelaInicial.java
@@ -2,6 +2,8 @@ package com.example.cf;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class TelaInicial extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class TelaInicial extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_inicial);
+    }
+
+    public void fisicas(View v) {
+        Toast.makeText(this, "Indo pra tela físicas", Toast.LENGTH_SHORT).show();
+    }
+    public void intelectuais(View v) {
+        Toast.makeText(this, "Indo pra tela intelectuais", Toast.LENGTH_SHORT).show();
+    }
+    public void neurodivergentes(View v) {
+        Toast.makeText(this, "Indo pra tela neurodivergentes", Toast.LENGTH_SHORT).show();
+    }
+    public void sensoriais(View v) {
+        Toast.makeText(this, "Indo pra tela sensoriais", Toast.LENGTH_SHORT).show();
     }
 }
