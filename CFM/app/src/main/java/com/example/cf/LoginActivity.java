9eed49665de7d1a529b38782b03af4e820de670f
luizinho2008 <luizhenriquemoreiracouto@gmail.com>
2025-06-16 14:28:24 -0300

    Começo da criação da tela de login

---
+++ CFM/app/src/main/java/com/example/cf/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.example.cf;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+//        TextView signUp = findViewById(R.id.textSignUp);
+//        signUp.setOnClickListener(v -> {
+//            Intent intent = new Intent(LoginActivity.this, CadastroActivity.class);
+//            startActivity(intent);
+//        });
+    }
+}
