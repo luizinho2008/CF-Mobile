4c538bc34ec4135bacca36cd1dabc5164da2c90c
luizinho2008 <luizhenriquemoreiracouto@gmail.com>
2025-07-28 20:54:36 -0300

    Criação das telas das deficiências físicas

--- CFM/app/src/main/java/com/example/cf/TelaFisica.java
+++ CFM/app/src/main/java/com/example/cf/TelaFisica.java
@@ -3,6 +3,7 @@ package com.example.cf;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,18 +14,18 @@ public class TelaFisica extends AppCompatActivity {
         setContentView(R.layout.activity_tela_fisica); // Confirme que o nome está certo e bate com o XML
     }
     public void abrirAmputacao(View view) {
-//        startActivity(new Intent(this, AmputacaoActivity.class));
+        startActivity(new Intent(this, Amputacao.class));
     }
 
     public void abrirNanismo(View view) {
-//        startActivity(new Intent(this, NanismoActivity.class));
+        startActivity(new Intent(this, Nanismo.class));
     }
 
     public void abrirPlegia(View view) {
-//        startActivity(new Intent(this, PlegiaActivity.class));
+        startActivity(new Intent(this, Plegia.class));
     }
 
     public void abrirPoliomielite(View view) {
-//        startActivity(new Intent(this, PoliomieliteActivity.class));
+        startActivity(new Intent(this, Poliomielite.class));
     }
 }
